6d8ac127a9e64971592782c6e20abd651f5d632a
bananza13 <164437055+bananza13@users.noreply.github.com>
2025-03-08 20:49:20 +0900

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,6 +20,7 @@ LINKS = (
     ("Pelican", "https://getpelican.com/"),
     ("Python.org", "https://www.python.org/"),
     ("GitHub", "https://github.com/"),
+    ("Wikipedia", "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D0%B0%D0%BC%D0%BE%D0%BB%D1%91%D1%82%D0%BE%D0%B2_%D0%92%D1%82%D0%BE%D1%80%D0%BE%D0%B9_%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%BE%D0%B9_%D0%B2%D0%BE%D0%B9%D0%BD%D1%8B",
 )
 
 # Social widget
